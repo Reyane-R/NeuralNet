6873db7bc4d01c946b2282a4c4e1a7e82bfadf51
Reyane-R <41648115+Reyane-R@users.noreply.github.com>
2022-01-29 13:57:39 +0100

    3 weightsets and 3 biases

--- p2.py
+++ p2.py
@@ -1,7 +1,13 @@
 
 inputs = [1, 2, 3 , 2.5]
-weights = [0.2, 0.8, -0.5, 1.0]
-bias = 2
+
+weights1 = [0.2, 0.8, -0.5, 1.0]
+weights2 = [0.5, -0.91, -0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
 
 outputs = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + inputs[3]*weights[3] + bias
 print(outputs)
